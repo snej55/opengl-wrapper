116ac4433173892ab1b334fcc1207aebeabf2037
Jens Kromdijk <jenskromdijk@gmail.com>
2025-04-06 18:06:29 +0100

    Transfer from geometry shader

--- main.cpp
+++ main.cpp
@@ -6,12 +6,11 @@ int main()
     App app{640, 640, "OpenGL window"};
     app.enableDepthTesting();
 
-    float points[]
-    {
-        -0.5f, 0.5f, // top-left
-        0.5f, 0.5f, // top-right
-        0.5f, -0.5f, // bottom-right
-        -0.5f, -0.5f // bottom-left
+    const float points[] = {
+        -0.5f, 0.5f, 1.0f, 0.0f, 0.0f, // top-left
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, // top-right
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // bottom-right
+        -0.5f, -0.5f, 1.0f, 1.0f, 0.0f // bottom-left
     };
 
     unsigned int VAO, VBO;
@@ -22,8 +21,10 @@ int main()
     glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
 
     glBindVertexArray(VAO);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), reinterpret_cast<void*>(0));
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), reinterpret_cast<void*>(0));
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), reinterpret_cast<void*>(0));
+    glEnableVertexAttribArray(1);
     glBindVertexArray(0);
 
     const Shader pointShader {"shaders/points.vert", "shaders/points.frag"};
